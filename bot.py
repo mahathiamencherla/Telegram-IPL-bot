767cbad2b790efcd82d6da9d8087d63f1db3d0e6
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-09-30 18:39:23 +0530

    Ready for deployment

--- bot.py
+++ bot.py
@@ -2,9 +2,14 @@ import requests
 import json
 import configparser as cfg
 
+import os
+from os import environ
+
+Telegram_Token = environ['TOKEN']
+
 class telegram_chatbot():
 	def __init__(self,config):
-		self.token = self.read_token_from_config_file(config)
+		self.token = self.Telegram_Token or self.read_token_from_config_file(config)
 		self.base = "https://api.telegram.org/bot{}/".format(self.token)
 
 	def get_updates(self, offset=None):
