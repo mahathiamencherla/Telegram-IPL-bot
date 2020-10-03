360ba9e5200dbaddfd92e6891465bfd3b9d04c73
manugeorge04 <manugeorge004@gmail.com>
2020-10-03 16:03:38 +0530

    Check curr time

--- server.py
+++ server.py
@@ -98,9 +98,9 @@ def make_reply(msg, id):
 		start_time_num = start_time.replace(":",'')
 		start_time_num = int(start_time_num)
 		if curr_time > start_time_num and curr_time <= 2359:
-			reply = currUpdates()
+			reply = curr_time#currUpdates()
 		else:
-			reply = match_day_details(True)
+			reply = curr_time#match_day_details(True)
 	else:
 		reply = "Welcome to the IPL Updates bot!\n\nUse this as a user guide:\n1. /next_match to get upcoming match details.\n2. /points_table to get updates points table.\n3. The bot is going to send you summary updates of every match. If you are prompted with a \"Do you want more match details?\" question, you can answer yes to receive over by over and wicket updates.\n4. Type \"Give me updates\" to get the current livescore. \n5. /get_updates to get over by over updates.  \n6. /stop_match_details to stop getting over by over and wicket details.\nYou can always use /stop to stop the bot.\n\nThank you!\n\n\nFind the source code on: https://github.com/mahathiamencherla15/Telegram-IPL-bot/ "	
 	return reply	
@@ -259,9 +259,9 @@ def set_toss_schedule():
 
 set_toss_schedule()
 #no change to thread except- starting it at start_time
-schedule.every().day.at(start_time).do(beginThread)
+#schedule.every().day.at(start_time).do(beginThread)
 
-# beginThread()
+beginThread()
 
 while True:
 	schedule.run_pending()
