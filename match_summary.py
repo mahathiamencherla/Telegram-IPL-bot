b89b3fc0ea9f8d9a95171536bbba1b71ba976ce8
manugeorge04 <manugeorge004@gmail.com>
2020-10-03 17:39:38 +0530

    Add IPs

--- match_summary.py
+++ match_summary.py
@@ -5,7 +5,7 @@ proxies = {
 	"http": 'http://185.175.76.21:80', 
     "https": 'http://58.11.59.192:80',
     "http": 'http://200.137.197.254:80',
-    "https": 'http://37.221.197.207:8080'
+    "https": 'http://103.231.218.126:53281'
 }
 
 def get_MOTM(match_id):
